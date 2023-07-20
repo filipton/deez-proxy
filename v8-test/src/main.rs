3f116aab77ab02071f6f1846ab1b6efcb0ade57e
filipton <filipton12@gmail.com>
2023-07-20 22:04:24 +0200

    feat: console.log

--- v8-test/src/main.rs
+++ v8-test/src/main.rs
@@ -1,3 +1,5 @@
+use std::sync::{Arc, Mutex};
+
 use anyhow::Result;
 
 #[derive(serde::Deserialize, Debug)]
@@ -21,17 +23,25 @@ fn main() -> Result<()> {
     let scope = &mut v8::ContextScope::new(scope, context);
     let global = context.global(scope);
 
+    let console_key = v8::String::new(scope, "console").unwrap();
+    let console_val = v8::Object::new(scope);
+    global.set(scope, console_key.into(), console_val.into());
+
+    set_func(scope, console_val, "log", console_log);
+
     let code = r#"
         async function run(a, b) {
+            console.log("dsdasdsa", "vcx");
+
             return {
                 what: "the fuck",
                 whats: "going on",
-                age: 19
+                age: a + b
             };
         }
     "#;
-    let code = v8::String::new(scope, &format!("{}\nrun", code)).unwrap();
 
+    let code = v8::String::new(scope, &format!("{}\nrun", code)).unwrap();
     let script = v8::Script::compile(scope, code, None).unwrap();
     let function = script.run(scope).unwrap();
     let function = v8::Local::<v8::Function>::try_from(function).unwrap();
@@ -48,10 +58,41 @@ fn main() -> Result<()> {
 
     let result = promise.result(scope).to_object(scope);
     if let Some(result) = result {
-        // parse result as serde struct
-        let test: TestStruct = serde_v8::from_v8(scope, result.into()).unwrap();
-        println!("{:?}", test);
+        let result_res: Result<TestStruct, serde_v8::Error> =
+            serde_v8::from_v8(scope, result.into());
+
+        if let Ok(result) = result_res {
+            println!("{:?}", result);
+        }
     }
 
     Ok(())
+}
+
+#[inline(always)]
+pub fn set_func(
+    scope: &mut v8::HandleScope<'_>,
+    obj: v8::Local<v8::Object>,
+    name: &'static str,
+    callback: impl v8::MapFnTo<v8::FunctionCallback>,
+) {
+    let key = v8::String::new(scope, name).unwrap();
+    let tmpl = v8::FunctionTemplate::new(scope, callback);
+    let val = tmpl.get_function(scope).unwrap();
+    val.set_name(key);
+    obj.set(scope, key.into(), val.into());
+}
+
+fn console_log(
+    scope: &mut v8::HandleScope,
+    args: v8::FunctionCallbackArguments,
+    mut rv: v8::ReturnValue,
+) {
+    let mut s = String::new();
+    for i in 0..args.length() {
+        let arg = args.get(i).to_rust_string_lossy(scope);
+        s.push_str(&format!("{} ", arg));
+    }
+    println!("{}", s);
+    rv.set(v8::undefined(scope).into());
 }
