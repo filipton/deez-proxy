28e946011da9a75bc7267b6fb788264e1a166def
filipton <filipton12@gmail.com>
2023-07-20 12:57:18 +0200

    feat: better error handling

--- src/main.rs
+++ src/main.rs
@@ -12,46 +12,62 @@ async fn main() -> Result<()> {
     println!("Listening on: {}", addr);
 
     loop {
-        let (mut socket, _) = listener.accept().await?;
+        let socket_res = listener.accept().await;
 
-        tokio::spawn(async move {
-            let mut out_stream = TcpStream::connect("192.168.1.1:80").await.unwrap();
-
-            let mut in_buf = vec![0; 4096];
-            let mut out_buf = vec![0; 4096];
-
-            loop {
-                tokio::select! {
-                    res = socket.read(&mut in_buf) => {
-                        if let Ok(n) = res {
-                            if n == 0 {
-                                continue;
-                            }
-                            let res = out_stream.write_all(&in_buf[..n]).await;
-
-                            if let Err(e) = res {
-                                println!("In Error: {}", e);
-                                break;
-                            }
-                        }
+        match socket_res {
+            Ok((socket, _addr)) => {
+                tokio::spawn(async move {
+                    if let Err(e) = handle_client(socket).await {
+                        println!("Handle Client Error: {}", e);
                     }
-                    res = out_stream.read(&mut out_buf) => {
-                        if let Ok(n) = res {
-                            if n == 0 {
-                                continue;
-                            }
-                            let res = socket.write_all(&out_buf[..n]).await;
+                });
+            }
+            Err(e) => {
+                println!("Bind Socket Error: {}", e);
+            }
+        }
+    }
+}
 
-                            if let Err(e) = res {
-                                println!("Out Error: {}", e);
-                                break;
-                            }
-                        }
+async fn handle_client(mut socket: TcpStream) -> Result<()> {
+    let mut out_stream = TcpStream::connect("192.168.1.1:80").await?;
+
+    let mut in_buf = vec![0; 4096];
+    let mut out_buf = vec![0; 4096];
+
+    loop {
+        tokio::select! {
+            res = socket.read(&mut in_buf) => {
+                if let Ok(n) = res {
+                    if n == 0 {
+                        break;
+                    }
+                    let res = out_stream.write_all(&in_buf[..n]).await;
+
+                    if let Err(e) = res {
+                        println!("In Error: {}", e);
+                        break;
                     }
                 }
             }
+            res = out_stream.read(&mut out_buf) => {
+                if let Ok(n) = res {
+                    if n == 0 {
+                        break;
+                    }
+                    let res = socket.write_all(&out_buf[..n]).await;
 
-            tokio::time::sleep(tokio::time::Duration::from_millis(1)).await;
-        });
+                    if let Err(e) = res {
+                        println!("Out Error: {}", e);
+                        break;
+                    }
+                }
+            }
+            else => {
+                println!("Else");
+            }
+        }
     }
+
+    Ok(())
 }
