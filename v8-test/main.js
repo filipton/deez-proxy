bc954d7ac93810b2fb341bb1e689e7026dba8ce1
filipton <filipton12@gmail.com>
2023-07-30 14:12:29 +0200

    chore: tokio as workspace dependency

--- v8-test/main.js
+++ v8-test/main.js
@@ -1,19 +1,18 @@
 async function handle(req) {
     try {
-        sleep(5000);
-
         if (req.port == 7071) {
             return {
                 hang_connection: true,
                 //block_connection: true, // same as hang_connection but without the 30s sleep
             }
-        } else if (req.port == 7072) {
+        } else if (req.port == 7070) {
             return {
                 ip: "vps.filipton.space:80",
                 no_delay: true, // if you want to proxy more advanced protocols, you need to enable nodelay
             }
         }
 
+        // sleep(5000);
         return {
             ip: "vps.filipton.space:25565",
             no_delay: true,
